a5fa46d313032313db03c9c1058202a0ff3b7d4d
ravizhan <ravizhan@hotmail.com>
2025-12-20 01:05:22 +0800

    更新资源复制脚本，修正接口文件路径并添加代理配置

--- scripts/copy_resources.py
+++ scripts/copy_resources.py
@@ -6,7 +6,17 @@ os.makedirs("build", exist_ok=True)
 shutil.copytree("agent", "build/agent")
 shutil.copytree("assets/resource","build/resource")
 shutil.copy2("assets/interface.json","build")
-with open("assets/interface.json", "r", encoding="utf-8") as f:
+with open("build/interface.json", "r", encoding="utf-8") as f:
     interface = json.load(f)
 interface["version"] = "3.17.4"
 interface["custom_title"] = f"M9A 3.17.4 | 亿韭韭韭小助手"
+interface["agent"] = {
+        "child_exec": "./python/python.exe",
+        "child_args": [
+            "-u",
+            "./agent/main.py"
+        ],
+        "timeout": -1
+    },
+with open("build/interface.json", "w", encoding="utf-8") as f:
+    json.dump(interface, f, ensure_ascii=False, indent=4)
